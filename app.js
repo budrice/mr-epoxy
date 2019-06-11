a15a595603e0c04940961270ea7dd8717c41d4f5
buddy <r.eldis@yahoo.com>
2019-06-11 18:44:18 -0400

    modified server response header

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ app.enable('trust proxy');
 app.all('/*', function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-type, Accept, X-Access-Token, X-Key');
+
+   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept, X-Access-Token, X-Key');
+
+  // res.header('Access-Control-Allow-Headers', 'Content-type, Accept, X-Access-Token, X-Key');
   if (req.method == 'OPTIONS') {
     res.status(200).end();
   } else {
